83e7eeaf5b4edbd5be874b38cbd79efb100ee4d7
Death Click <60820510+bookick@users.noreply.github.com>
2020-10-28 10:03:16 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -125,7 +125,7 @@ $('#yes').click(function() {
                 text: "Tối nay tớ qua đón cậu đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo",
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
-                    window.location = 'http://fb.com';
+                    window.location = 'https://www.facebook.com/messages/t/100037481748524';
                   }
             })
         }
